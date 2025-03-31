99832c3ecfd979decce89d92c4689ecc176369f7
backend <Cavidbabayev118@gmail.com>
2025-03-31 13:58:04 +0400

    elave edildi

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 40;
+            int b = 50;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
